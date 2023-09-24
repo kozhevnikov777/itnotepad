4cee166dbfbdd65c4ac1fba44ccf25d8cbfce123
kozhevnikov777 <vkozhevnikov777@mail.ru>
2023-09-25 01:39:12 +0300

    modified 	modified:   app/Http/Controllers/Admin/Post/IndexController.php

--- app/Http/Controllers/Admin/Post/IndexController.php
+++ app/Http/Controllers/Admin/Post/IndexController.php
@@ -17,11 +17,9 @@ class IndexController extends BaseController
         $time = array();
         $option = array();
         foreach($posts as $post){
-            // dd();
             array_push($time, Carbon::parse($post->end_time)->diff(Carbon::parse($post->start_time))->format('%h:%I'));
             array_push($option, 0);
         }
-        // dd($time);
         return view('admin.post.main', compact('posts','time', 'option'));
     }
 }
