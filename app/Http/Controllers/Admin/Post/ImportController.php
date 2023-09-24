9e52130e2247d97b16d07d48301d0efe7a35cb77
kozhevnikov777 <vkozhevnikov777@mail.ru>
2023-09-25 00:08:42 +0300

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch main  Your branch is up-to-date with 'origin/main'.
     Changes to be committed:
    	new file:   .htaccess
    	modified:   app/Http/Controllers/Admin/Post/ImportController.php
    	modified:   public/plugins/summernote/font/summernote.woff
    
     Changes not staged for commit:
    	deleted:    composer.phar
    	deleted:    public/.htaccess

--- app/Http/Controllers/Admin/Post/ImportController.php
+++ app/Http/Controllers/Admin/Post/ImportController.php
@@ -3,12 +3,39 @@
 namespace App\Http\Controllers\Admin\Post;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class ImportController extends Controller
 {
     public function __invoke(Request $request)
     {
-        dd($request);
+        $file = $request->file('import_file');
+        if($file == null){
+            return redirect()->back();
+        }
+
+        if ($file->getClientOriginalExtension() === 'json') {
+            $json = file_get_contents($file);
+            $data = json_decode($json, true);
+
+            foreach ($data as $element) {
+                $post = new Post;
+                $post->title = $element['title'];
+                $post->content = $element['content'];
+                $post->start_time = $element['start_time'];
+                $post->end_time = $element['end_time'];
+                $post->category_id = $element['category_id'];
+                $post->created_at = $element['created_at'];
+                $post->updated_at = $element['updated_at'];
+                $post->deleted_at = $element['deleted_at'];
+                $post->user_id = $element['user_id'];
+                $post->save();
+
+            }
+            return back();
+        }
+
+        return redirect()->back();
     }
 }
